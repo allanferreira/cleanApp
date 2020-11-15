9a5d69644025d0f612b5501b7b38d1fa8593e174
Allan Ferreira <allan.less@outlook.com>
2020-11-15 00:03:27 -0300

    ensure HttpPostClient not complete when RemoteAddAccount has been deallocated

--- Data/UseCases/RemoteAddAccount.swift
+++ Data/UseCases/RemoteAddAccount.swift
@@ -11,7 +11,8 @@ public final class RemoteAddAccount: AddAccount {
     }
     
     public func add(addAccount: AddAccountModel, completion: @escaping (Result<AccountModel, DomainError>) -> Void) {
-        httpClient.post(to: url, with: addAccount.toData()) { result in
+        httpClient.post(to: url, with: addAccount.toData()) { [weak self] result in
+            guard self != nil else {return}
             switch result {
             case .success(let data):
                 if let model: AccountModel = data.toModel() {
